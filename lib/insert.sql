3c277a84d30d1f0ad8c4ead731b4ea1b63a8a0ca
Sam Arita <arita.sam@student.moringaschool.com>
2022-10-28 16:10:18 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1,'Mr. Chocolate', 4, 'M', 'Brown', 'Angry', TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 4, 'M', 'Brown', 'Angry', TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 4, 'F', 'Brown', 'Angry', TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 4, 'M', 'Brown', 'Angry', TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 4, 'F', 'Brown', 'Angry', TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 4, 'M', 'Brown', 'Angry', TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 4, 'F', 'Brown', 'Angry', TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 4, 'M', 'Brown', 'Angry', TRUE);
